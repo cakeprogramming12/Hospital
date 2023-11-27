8e9c8f9b6d51d3dc509e7be491412a79b19b8c18
Ramiro Vega <110796808+cakeprogramming12@users.noreply.github.com>
2023-11-27 00:51:44 -0600

    nuevas cosas

--- Login/sesion.php
+++ Login/sesion.php
@@ -5,22 +5,30 @@ require 'conexion.php';
 session_start();
 
 /*
-Declaramos las variables para almacenar el valor de los imputs
+Declaramos las variables para almacenar el valor de los inputs
 */
 $usuario=$_POST['user'];
 $clave=$_POST['pass'];
 
+
+#hacemos la consulta a la tabla que contenga los usuarios
 $query=("SELECT * FROM usuarios 
 	WHERE usuario='$usuario' AND contrasena='$clave'");
 
+#Ejecutamos la consulta, pasando la conexion y el query
 $consulta=pg_query($conexion,$query);
+#numero de columnas que salieron de esa consulta
 $cantidad=pg_num_rows($consulta);
 
+#si salio un registro
 if($cantidad>0){
 
+    #variable de sesion, para poder usarlo en otra paguina
 	$_SESSION['nombre_usuario']=$usuario;
-	header('Location:ingreso.php');
+	#nos dirige a otra paguina
+    header('Location:../tablero/tablero.html');
 }
+#los datos estan mal
 else{
 	echo "Datos incorrectos.";
 }
