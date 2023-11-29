c41e4f8c6766d336415db47ef03d8c9aad2ff6bd
anmartinezm2 <111255059+anmartinezm2@users.noreply.github.com>
2023-11-28 20:48:27 -0600

    hospital.sql

--- querys/hospital.sql
+++ querys/hospital.sql
@@ -208,21 +208,22 @@ FOREIGN KEY (Id_departamento) REFERENCES departamentos(Id_departamento));
 
 
 
-CREATE TABLE expediente(
-Id_expediente INT PRIMARY KEY,
-Diagnosticos VARCHAR(50) NOT NULL,
-Tratamientos VARCHAR(50) NOT NULL,
-Intervenciones_quirurgicas VARCHAR(50) NOT NULL,
-Sintomas VARCHAR (50) NOT NULL,
-Antecedentes VARCHAR(100) NOT NULL,
-F_ingreso DATE NOT NULL,
-F_egreso DATE,
-Descripcion VARCHAR(150) NOT NULL,
-Id_paciente INT,
-Id_departamento INT,
-FOREIGN KEY (Id_paciente) REFERENCES pacientes(Id_paciente),
-FOREIGN KEY (Id_departamento) REFERENCES departamentos(Id_departamento))
-PARTITION BY RANGE (F_ingreso);
+CREATE TABLE expediente( 
+    Id_expediente INT, 
+    Diagnosticos VARCHAR(50) NOT NULL, 
+    Tratamientos VARCHAR(50) NOT NULL, 
+    Intervenciones_quirurgicas VARCHAR(50) NOT NULL, 
+    Sintomas VARCHAR(50) NOT NULL, 
+    Antecedentes VARCHAR(100) NOT NULL, 
+    F_ingreso DATE NOT NULL, 
+    F_egreso DATE, 
+    Descripcion VARCHAR(150) NOT NULL, 
+    Id_paciente INT, 
+    Id_departamento INT, 
+    PRIMARY KEY (Id_expediente, F_ingreso),  
+    FOREIGN KEY (Id_paciente) REFERENCES pacientes(Id_paciente), 
+    FOREIGN KEY (Id_departamento) REFERENCES departamentos(Id_departamento) 
+) PARTITION BY RANGE (F_ingreso); 
 
 
 CREATE TABLE expediente_2020 PARTITION OF expediente FOR VALUES FROM ('2020-01-01')
